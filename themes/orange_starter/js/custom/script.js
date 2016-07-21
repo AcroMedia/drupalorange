82964e9287bda6dd4dafb10d90ed33ee4518afe6
amberbam <abahm@acromediainc.com>
2016-07-21 16:59:35 -0700

    Remove carousel image zoom, add colour hover variables, add variable and class names to live CSS, print out font being used

--- themes/orange_starter/js/custom/script.js
+++ themes/orange_starter/js/custom/script.js
@@ -30,4 +30,27 @@
   //Double Tap parent of dropdown on touch devices
   $('.menu__dropdown-parent').doubleTapToGo();
 
+
+  //TYPOGRAPHY
+  //Print the font size and family
+  var heading = $('.typography :header'),
+    headingDescriptionText = heading.children('span').eq(0),
+    body = heading.next('p'),
+    bodyDescriptionText = body.children('span').eq(0);
+
+  setTypography(heading, headingDescriptionText);
+  setTypography(body, bodyDescriptionText);
+  $(window).on('resize', function(){
+    setTypography(heading, headingDescriptionText);
+    setTypography(body, bodyDescriptionText);
+  });
+
+  function setTypography(element, textElement) {
+    var fontSize = Math.round(element.css('font-size').replace('px',''))+'px',
+      fontFamily = (element.css('font-family').split(','))[0].replace(/\'/g, '').replace(/\"/g, ''),
+      fontWeight = element.css('font-weight');
+    textElement.text(fontWeight + ' '+ fontFamily+' '+fontSize );
+  }
+
+
 })(jQuery, Drupal);
