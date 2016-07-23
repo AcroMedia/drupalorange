a4eb8407204cf828dff3d3d9109224aba974f5f0
amberbam <abahm@acromediainc.com>
2016-07-22 18:09:54 -0700

    Modal animation, printing font names, etc

--- themes/orange_starter/js/custom/script.js
+++ themes/orange_starter/js/custom/script.js
@@ -30,27 +30,19 @@
   //Double Tap parent of dropdown on touch devices
   $('.menu__dropdown-parent').doubleTapToGo();
 
+  //Add table class to all of the tables added to content
+  $('.content__main-content table').addClass('table');
 
-  //TYPOGRAPHY
-  //Print the font size and family
-  var heading = $('.typography :header'),
-    headingDescriptionText = heading.children('span').eq(0),
-    body = heading.next('p'),
-    bodyDescriptionText = body.children('span').eq(0);
+  //Make tables responsive
+  $('.content__main-content .table').wrap('<div class="table-responsive"></div>');
 
-  setTypography(heading, headingDescriptionText);
-  setTypography(body, bodyDescriptionText);
-  $(window).on('resize', function(){
-    setTypography(heading, headingDescriptionText);
-    setTypography(body, bodyDescriptionText);
+  //Add classes to aligned images in content to add styles
+  $('.content__main-content img').each(function() {
+    var float = $(this).css('float');
+    if(float == 'right')
+      $(this).addClass('img-right');
+    else if(float == 'left')
+      $(this).addClass('img-left');
   });
-
-  function setTypography(element, textElement) {
-    var fontSize = Math.round(element.css('font-size').replace('px',''))+'px',
-      fontFamily = (element.css('font-family').split(','))[0].replace(/\'/g, '').replace(/\"/g, ''),
-      fontWeight = element.css('font-weight');
-    textElement.text(fontWeight + ' '+ fontFamily+' '+fontSize );
-  }
-
 
 })(jQuery, Drupal);
